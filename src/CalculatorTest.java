af7a4e53b65b0b6e0015b3b2925ad37a5ae2803f
Gilbert <ssenyonjogilbert@gmail.com>
2021-05-18 07:27:32 +0300

    Implemented CalculatorTest using JUnit

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,24 +1,12 @@
-public class CalculatorTest {
-    private int nbErrors = 0;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class CalculatorTest {
+
+    @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        double result = calculator.add(109, 50);
-        if(result != 60) {
-            throw new IllegalStateException("Bad result " + result);
-        }
-    }
-
-    public static void main(String[] args) {
-        var test = new CalculatorTest();
-        try {
-            test.testAdd();
-        } catch (Throwable e) {
-            test.nbErrors++;
-            e.printStackTrace();
-        }
-        if(test.nbErrors > 0) {
-            throw new IllegalStateException("There were " + test.nbErrors + " errors");
-        }
+        double result = calculator.add(20, 40);
+        assertEquals(60, result, 0);
     }
 }
