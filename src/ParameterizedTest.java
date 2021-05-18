bd46d86216df675728d8d7f8f19b39cb416dc6d2
Gilbert <ssenyonjogilbert@gmail.com>
2021-05-18 08:13:14 +0300

    Implemented ParameterizedTest

---
+++ src/ParameterizedTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(value= Parameterized.class)
+public class ParameterizedTest {
+    private double expected;
+    private double valueOne;
+    private double valueTwo;
+
+
+    @Parameterized.Parameters
+    public static Collection<Integer[]> getTestParameters() {
+        return Arrays.asList(new Integer[][] {
+                {2, 1, 1}, // expected, valueOne, valueTwo
+                {3, 2, 1}, // expected, valueOne, valueTwo
+                {4, 3, 1}, // expected, valueOne, valueTwo
+        });
+    }
+
+    public ParameterizedTest(double expected, double valueOne, double valueTwo) {
+        this.expected = expected;
+        this.valueOne = valueOne;
+        this.valueTwo = valueTwo;
+    }
+
+    @Test
+    public void sum() {
+        Calculator calc = new Calculator();
+        assertEquals(expected, calc.add(valueOne, valueTwo), 0);
+    }
+}
