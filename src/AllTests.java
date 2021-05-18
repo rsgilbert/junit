bef6a019b78377a0c968e9811a125b170ee55a55
Gilbert <ssenyonjogilbert@gmail.com>
2021-05-18 08:16:49 +0300

    Implemented a test suite called AllTests

---
+++ src/AllTests.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(value= Suite.class)
+@Suite.SuiteClasses(value={CalculatorTest.class, ParameterizedTest.class})
+public class AllTests {
+}
